4bc5980d6b747cb1f96962b73c988f82f85d66e0
matteo <matteo.faura-behague@etu.umontpellier.fr>
2022-09-15 16:44:50 +0200

    Axe's Sounds and Particles adjustment, and comments.

--- src/main/java/fr/inuripse/naturerain/item/toolandweapon/LeafyZirmsAxe.java
+++ src/main/java/fr/inuripse/naturerain/item/toolandweapon/LeafyZirmsAxe.java
@@ -45,11 +45,15 @@ public class LeafyZirmsAxe extends AxeItem {
         Player pPlayer = pContext.getPlayer();
         if (applyBonemeal(holdItem, level, blockpos, pContext.getPlayer())) {
             level.playSound(pPlayer, blockpos1, SoundEvents.WET_GRASS_PLACE, SoundSource.BLOCKS, 1.0F, 2.6F + (level.random.nextFloat() - level.random.nextFloat()) * 0.8F);
-            holdItem.hurtAndBreak(5, pPlayer, (player) -> player.broadcastBreakEvent(holdItem.getEquipmentSlot()));
-            pPlayer.getCooldowns().addCooldown(this, 20);
-            if (!level.isClientSide) {
-                ((ServerLevel)level).sendParticles(ParticleTypes.RAIN, blockpos.getX(), blockpos.getY(), blockpos.getZ(), 30, 1.5D,1.0D,1.5D,1);
-                level.levelEvent(1505, blockpos, 0);
+            if(pPlayer!=null) {
+                pPlayer.getCooldowns().addCooldown(this, 20);
+                if (!level.isClientSide) {
+                    ((ServerLevel) level).sendParticles(ParticleTypes.RAIN, blockpos.getX(), blockpos.getY(), blockpos.getZ(), 30, 1.5D, 1.0D, 1.5D, 1);
+                    level.levelEvent(1505, blockpos, 0);
+                    pContext.getItemInHand().hurtAndBreak(5, pPlayer, (player) -> {
+                        player.broadcastBreakEvent(pContext.getHand());
+                    });
+                }
             }
             return InteractionResult.sidedSuccess(level.isClientSide);
         } else {
@@ -57,10 +61,13 @@ public class LeafyZirmsAxe extends AxeItem {
             boolean flag = blockstate.isFaceSturdy(level, blockpos, pContext.getClickedFace());
             if (flag && growWaterPlant(level, blockpos1, pContext.getClickedFace())) {
                 level.playSound(pPlayer, blockpos1, SoundEvents.WET_GRASS_PLACE, SoundSource.BLOCKS, 1.0F, 2.6F + (level.random.nextFloat() - level.random.nextFloat()) * 0.8F);
-                holdItem.hurtAndBreak(5, pPlayer, (player) -> player.broadcastBreakEvent(holdItem.getEquipmentSlot()));
-                pPlayer.getCooldowns().addCooldown(this, 20);
-                if (!level.isClientSide) {
-                    level.levelEvent(1505, blockpos, 0);
+                if(pPlayer!=null) {
+                    pPlayer.getCooldowns().addCooldown(this, 20);
+                    if (!level.isClientSide) {
+                        level.levelEvent(1505, blockpos, 0);
+                        pContext.getItemInHand().hurtAndBreak(5, pPlayer, (player) -> {
+                            player.broadcastBreakEvent(pContext.getHand());
+                        });                    }
                 }
                 return InteractionResult.sidedSuccess(level.isClientSide);
             } else {
@@ -88,64 +95,63 @@ public class LeafyZirmsAxe extends AxeItem {
         return false;
     }
 
-    public static boolean growWaterPlant(Level pLevel, BlockPos pPos, @Nullable Direction pClickedSide) {
+    private static boolean growWaterPlant(Level pLevel, BlockPos pPos, @Nullable Direction pClickedSide) {
         if (pLevel.getBlockState(pPos).is(Blocks.WATER) && pLevel.getFluidState(pPos).getAmount() == 8) {
             if (!(pLevel instanceof ServerLevel)) {
                 return true;
-            } else {
-                Random random = pLevel.getRandom();
+            }
+            Random random = pLevel.getRandom();
 
-                label78:
-                for(int i = 0; i < 128; ++i) {
-                    BlockPos blockpos = pPos;
-                    BlockState blockstate = Blocks.SEAGRASS.defaultBlockState();
+            label78:
+            for(int i = 0; i < 128; ++i) {
+                BlockPos blockpos = pPos;
+                BlockState blockstate = Blocks.SEAGRASS.defaultBlockState();
 
-                    for(int j = 0; j < i / 16; ++j) {
-                        blockpos = blockpos.offset(random.nextInt(3) - 1, (random.nextInt(3) - 1) * random.nextInt(3) / 2, random.nextInt(3) - 1);
-                        if (pLevel.getBlockState(blockpos).isCollisionShapeFullBlock(pLevel, blockpos)) {
-                            continue label78;
-                        }
-                    }
-
-                    Holder<Biome> holder = pLevel.getBiome(blockpos);
-                    if (holder.is(Biomes.WARM_OCEAN)) {
-                        if (i == 0 && pClickedSide != null && pClickedSide.getAxis().isHorizontal()) {
-                            blockstate = Registry.BLOCK.getTag(BlockTags.WALL_CORALS).flatMap((p_204098_) -> {
-                                return p_204098_.getRandomElement(pLevel.random);
-                            }).map((p_204100_) -> {
-                                return p_204100_.value().defaultBlockState();
-                            }).orElse(blockstate);
-                            if (blockstate.hasProperty(BaseCoralWallFanBlock.FACING)) {
-                                blockstate = blockstate.setValue(BaseCoralWallFanBlock.FACING, pClickedSide);
-                            }
-                        } else if (random.nextInt(4) == 0) {
-                            blockstate = Registry.BLOCK.getTag(BlockTags.UNDERWATER_BONEMEALS).flatMap((p_204091_) -> {
-                                return p_204091_.getRandomElement(pLevel.random);
-                            }).map((p_204095_) -> {
-                                return p_204095_.value().defaultBlockState();
-                            }).orElse(blockstate);
-                        }
-                    }
-
-                    if (blockstate.is(BlockTags.WALL_CORALS, (p_204093_) -> {
-                        return p_204093_.hasProperty(BaseCoralWallFanBlock.FACING);
-                    })) {
-                        for(int k = 0; !blockstate.canSurvive(pLevel, blockpos) && k < 4; ++k) {
-                            blockstate = blockstate.setValue(BaseCoralWallFanBlock.FACING, Direction.Plane.HORIZONTAL.getRandomDirection(random));
-                        }
-                    }
-
-                    if (blockstate.canSurvive(pLevel, blockpos)) {
-                        BlockState blockstate1 = pLevel.getBlockState(blockpos);
-                        if (blockstate1.is(Blocks.WATER) && pLevel.getFluidState(blockpos).getAmount() == 8) {
-                            pLevel.setBlock(blockpos, blockstate, 3);
-                        } else if (blockstate1.is(Blocks.SEAGRASS) && random.nextInt(10) == 0) {
-                            ((BonemealableBlock)Blocks.SEAGRASS).performBonemeal((ServerLevel)pLevel, random, blockpos, blockstate1);
-                        }
+                for(int j = 0; j < i / 16; ++j) {
+                    blockpos = blockpos.offset(random.nextInt(3) - 1, (random.nextInt(3) - 1) * random.nextInt(3) / 2, random.nextInt(3) - 1);
+                    if (pLevel.getBlockState(blockpos).isCollisionShapeFullBlock(pLevel, blockpos)) {
+                        continue label78;
                     }
                 }
-                return true;
+
+                Holder<Biome> holder = pLevel.getBiome(blockpos);
+                if (holder.is(Biomes.WARM_OCEAN)) {
+                    if (i == 0 && pClickedSide != null && pClickedSide.getAxis().isHorizontal()) {
+                        blockstate = Registry.BLOCK.getTag(BlockTags.WALL_CORALS).flatMap((p_204098_) -> {
+                            return p_204098_.getRandomElement(pLevel.random);
+                        }).map((p_204100_) -> {
+                            return p_204100_.value().defaultBlockState();
+                        }).orElse(blockstate);
+                        if (blockstate.hasProperty(BaseCoralWallFanBlock.FACING)) {
+                            blockstate = blockstate.setValue(BaseCoralWallFanBlock.FACING, pClickedSide);
+                        }
+                    } else if (random.nextInt(4) == 0) {
+                        blockstate = Registry.BLOCK.getTag(BlockTags.UNDERWATER_BONEMEALS).flatMap((p_204091_) -> {
+                            return p_204091_.getRandomElement(pLevel.random);
+                        }).map((p_204095_) -> {
+                            return p_204095_.value().defaultBlockState();
+                        }).orElse(blockstate);
+                    }
+                }
+
+                if (blockstate.is(BlockTags.WALL_CORALS, (p_204093_) -> {
+                    return p_204093_.hasProperty(BaseCoralWallFanBlock.FACING);
+                })) {
+                    for(int k = 0; !blockstate.canSurvive(pLevel, blockpos) && k < 4; ++k) {
+                        blockstate = blockstate.setValue(BaseCoralWallFanBlock.FACING, Direction.Plane.HORIZONTAL.getRandomDirection(random));
+                    }
+                }
+
+                if (blockstate.canSurvive(pLevel, blockpos)) {
+                    BlockState blockstate1 = pLevel.getBlockState(blockpos);
+                    if (blockstate1.is(Blocks.WATER) && pLevel.getFluidState(blockpos).getAmount() == 8) {
+                        pLevel.setBlock(blockpos, blockstate, 3);
+                    } else if (blockstate1.is(Blocks.SEAGRASS) && random.nextInt(10) == 0) {
+                        ((BonemealableBlock)Blocks.SEAGRASS).performBonemeal((ServerLevel)pLevel, random, blockpos, blockstate1);
+                    }
+                }
             }
+            return true;
         } else {
             return false;
         }
