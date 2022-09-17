8d488b3664aa9cf282ead5ee2b115ab73d1f30c6
matteo <matteo.faura-behague@etu.umontpellier.fr>
2022-09-17 10:18:22 +0200

    Remove not used imports.

--- src/main/java/fr/inuripse/naturerain/item/armor/renderer/SnailShellChestplateRenderer.java
+++ src/main/java/fr/inuripse/naturerain/item/armor/renderer/SnailShellChestplateRenderer.java
@@ -2,9 +2,6 @@ package fr.inuripse.naturerain.item.armor.renderer;
 
 import fr.inuripse.naturerain.item.armor.SnailShellChestplate;
 import fr.inuripse.naturerain.item.armor.model.SnailShellChestplateModel;
-import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.renderer.entity.RenderLayerParent;
-import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
 
 public class SnailShellChestplateRenderer extends GeoArmorRenderer<SnailShellChestplate> {
