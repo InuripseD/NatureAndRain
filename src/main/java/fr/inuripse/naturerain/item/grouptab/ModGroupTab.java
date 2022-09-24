a8d01a0ec634ce82af8395d3845e3a8de90d7807
matteo <matteo.faura-behague@etu.umontpellier.fr>
2022-09-24 23:04:27 +0200

    Add the enchanted book Defoliate to the item list.

--- src/main/java/fr/inuripse/naturerain/item/grouptab/ModGroupTab.java
+++ src/main/java/fr/inuripse/naturerain/item/grouptab/ModGroupTab.java
@@ -1,8 +1,15 @@
 package fr.inuripse.naturerain.item.grouptab;
 
+import fr.inuripse.naturerain.enchantment.ModEnchantments;
 import fr.inuripse.naturerain.item.ModItems;
+import net.minecraft.core.NonNullList;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import org.jetbrains.annotations.NotNull;
 
 public class ModGroupTab {
@@ -11,6 +18,12 @@ public class ModGroupTab {
         @Override
         public @NotNull ItemStack makeIcon() {
             return new ItemStack(ModItems.WET_LEAF.get());
+        }
+
+        @Override
+        public void fillItemList(NonNullList<ItemStack> pItems) {
+            super.fillItemList(pItems);
+            pItems.add(EnchantedBookItem.createForEnchantment(new EnchantmentInstance(ModEnchantments.DEFOLIATE.get(), 1)));
         }
     };
 
