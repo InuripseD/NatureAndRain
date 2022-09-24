6f8aa67b7070f850f82d77167d5f07219bc7d567
matteo <matteo.faura-behague@etu.umontpellier.fr>
2022-09-24 09:59:20 +0200

    KeyPressed Packet is register in instance!

--- src/main/java/fr/inuripse/naturerain/networking/ModMessages.java
+++ src/main/java/fr/inuripse/naturerain/networking/ModMessages.java
@@ -2,6 +2,7 @@ package fr.inuripse.naturerain.networking;
 
 import fr.inuripse.naturerain.NatureRain;
 import fr.inuripse.naturerain.networking.packet.ItemStackRendererSyncStoCpacket;
+import fr.inuripse.naturerain.networking.packet.KeyPressedC2Spacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.NetworkDirection;
@@ -31,6 +32,12 @@ public class ModMessages {
                 .decoder(ItemStackRendererSyncStoCpacket::new)
                 .encoder(ItemStackRendererSyncStoCpacket::toBytes)
                 .consumer(ItemStackRendererSyncStoCpacket::handle)
+                .add();
+
+        net.messageBuilder(KeyPressedC2Spacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
+                .decoder(KeyPressedC2Spacket::new)
+                .encoder(KeyPressedC2Spacket::toBytes)
+                .consumer(KeyPressedC2Spacket::handle)
                 .add();
     }
 
