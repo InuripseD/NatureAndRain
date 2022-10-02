1cf594dfe71828053f85ef1861655b4c73e04def
matteo <matteo.faura-behague@etu.umontpellier.fr>
2022-10-02 16:34:03 +0200

    Add the Lava Vision Potion to the Creative tab.

--- src/main/java/fr/inuripse/naturerain/item/grouptab/ModGroupTab.java
+++ src/main/java/fr/inuripse/naturerain/item/grouptab/ModGroupTab.java
@@ -2,13 +2,14 @@ package fr.inuripse.naturerain.item.grouptab;
 
 import fr.inuripse.naturerain.enchantment.ModEnchantments;
 import fr.inuripse.naturerain.item.ModItems;
+import fr.inuripse.naturerain.potion.ModPotions;
 import net.minecraft.core.NonNullList;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import org.jetbrains.annotations.NotNull;
 
@@ -23,6 +24,10 @@ public class ModGroupTab {
         @Override
         public void fillItemList(NonNullList<ItemStack> pItems) {
             super.fillItemList(pItems);
+            ItemStack itemStack = new ItemStack(Items.POTION.asItem());
+            itemStack.setTag(new CompoundTag());
+            PotionUtils.setPotion(itemStack, ModPotions.LAVA_VISION.get());
+            pItems.add(itemStack);
             pItems.add(EnchantedBookItem.createForEnchantment(new EnchantmentInstance(ModEnchantments.DEFOLIATE.get(), 1)));
         }
     };
