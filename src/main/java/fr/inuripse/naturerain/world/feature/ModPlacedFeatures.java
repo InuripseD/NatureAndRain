99506e482983ba5586820fd02066f43376dc7089
matteo <matteo.faura-behague@etu.umontpellier.fr>
2022-10-11 22:23:12 +0200

    Changed Zirms spawn rate and vain size based on config file.

--- src/main/java/fr/inuripse/naturerain/world/feature/ModPlacedFeatures.java
+++ src/main/java/fr/inuripse/naturerain/world/feature/ModPlacedFeatures.java
@@ -6,10 +6,12 @@ import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
+import static fr.inuripse.naturerain.config.NatureRainCommonConfigs.ZirmsVaineFrenquency;
+
 public class ModPlacedFeatures {
 
     public static final Holder<PlacedFeature> ZIRMS_ORE_PLACED = PlacementUtils.register("zirms_ore_placed",
-            ModConfiguredFeatures.ZIRMS_ORE,ModOrePlacements.commonOrePlacement(4,
+            ModConfiguredFeatures.ZIRMS_ORE,ModOrePlacements.commonOrePlacement(ZirmsVaineFrenquency.get(),
                     HeightRangePlacement.triangle(VerticalAnchor.absolute(0), VerticalAnchor.absolute(80))));
 
 }
